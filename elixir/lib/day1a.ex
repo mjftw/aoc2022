67dc184036aba7adfbd173bfb654a5dd5f5cbd3a
Merlin Webster <merlin.webster@multiverse.io>
2022-12-01 17:20:13 +0000

    Improve day1 ex

--- elixir/lib/day1a.ex
+++ elixir/lib/day1a.ex
@@ -1,5 +1,4 @@
 defmodule Day1a do
-  @spec solve(String.t()) :: :ok
   def solve(input_path) do
     {tail, head} =
       input_path
@@ -20,16 +19,11 @@ defmodule Day1a do
     IO.puts("Answer to part 2: #{answer_part2}")
   end
 
-  @spec reducer(String.t(), {[non_neg_integer()], non_neg_integer()}) ::
-          {[non_neg_integer()], non_neg_integer()}
-  defp reducer(line, {all_elves_totals, current_elf_calories}) do
-    case line do
-      "\n" ->
-        {[current_elf_calories | all_elves_totals], 0}
+  defp reducer(line, {all_elves_totals, current_elf_calories}) when line == "\n",
+    do: {[current_elf_calories | all_elves_totals], 0}
 
-      calories_string ->
-        {calories, _} = calories_string |> Integer.parse()
-        {all_elves_totals, current_elf_calories + calories}
-    end
+  defp reducer(line, {all_elves_totals, current_elf_calories}) do
+    {calories, _} = Integer.parse(line)
+    {all_elves_totals, current_elf_calories + calories}
   end
 end
